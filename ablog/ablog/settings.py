ced8ce583019a29a315939a658790bef04518a9b
krzysiek_knop <knop.krzysiek@gmail.com>
2022-01-04 20:23:55 +0100

    load static

--- ablog/ablog/settings.py
+++ ablog/ablog/settings.py
@@ -125,7 +125,10 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
